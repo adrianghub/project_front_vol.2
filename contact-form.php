20882df63344ab5a50a72e3cb8d8ba2e17df1033
Adrian <adrian_zinko@op.pl>
2019-07-18 07:16:06 +0200

    feat: comment out php syntax

--- contact-form.php
+++ contact-form.php
@@ -1,16 +1,23 @@
 <?php
 
 if (isset($_POST['submit'])) {
+  // set variables
   $name = $_POST['name'];
   $mailFrom = $_POST['email'];
   $message = $_POST['message'];
 
-  $mailTo = 'zinko.adrian00@gmail.com';
-  $headers = "From: ".$mailFrom; 
-  $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+  $mailTo = 'zinko.adrian00@gmail.com'; // my email
+  $headers = "From: ".$mailFrom; // visitor email
+  
+  // message sent to visitor
+  $txt = "You have received an e-mail from ".$name.".\n\n".$message; 
   $headers = "Reply-To: ".$mailTo.".\r\n";
 
+
+  // php function for email building structure
   mail($mailTo, $name, $txt, $headers);
+
+  // location of the contact form
   header("Location: index.html");
 }
 
